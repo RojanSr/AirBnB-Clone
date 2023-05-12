c9a70d9baa4dc78052b3faad4368e7eff1f83677
RojanSr <rairojan1116@gmail.com>
2023-05-12 18:43:21 +0545

    patched badge text status

--- components/Card.jsx
+++ components/Card.jsx
@@ -2,8 +2,17 @@ import React from "react";
 import Star from "../images/star.png";
 
 export default function Card(props) {
+  let badgeText;
+
+  if (props.item.openSpots === 0) {
+    badgeText = "SOLD OUT";
+  } else if (props.item.location === "Online") {
+    badgeText = "ONLINE";
+  }
+
   return (
     <div className="card" id="card">
+      {badgeText && <div className="card--status">{badgeText}</div>}
       <img src={"../images/" + props.item.coverImg} className="profile-img" />
 
       <div className="flex">
